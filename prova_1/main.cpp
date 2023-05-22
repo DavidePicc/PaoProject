20cc442273081a8f8f36768c344ad054e59a3be8
Davide Picello <119404292+DavidePicc@users.noreply.github.com>
2023-05-22 13:00:43 +0200

    Aggiunto il progetto "prova1" (Hello world) per verificare configurazione di build automation

---
+++ prova_1/main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+
+#include <QLabel>
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    // Defines an application object
+    QApplication app(argc, argv);
+    // A piece of text
+    QLabel hello("Hello world!");
+    // Tells Qt to actually show the label
+    hello.show();
+    // Runs application
+    return app.exec();
+}
