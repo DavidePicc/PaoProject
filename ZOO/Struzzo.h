b918a1017925647426f0f2f0f56775dd4d73fd38
mhdmd01 <119261970+mhdmd01@users.noreply.github.com>
2023-06-30 18:52:11 +0200

    Update Struzzo.h

--- ZOO/Struzzo.h
+++ ZOO/Struzzo.h
@@ -10,13 +10,9 @@ float velocitamax; //in km/h
 public:
     Struzzo(const std::string& nome, int eta, const char sesso, float peso, const std::string& cibo_preferito, std::string& descrizione, float velocitamax);
 
-    std::string getName() const override { 
-        //return "Struzzo"; 
-        }
-
-    void emettereVerso() const override {
-        //std::cout << "grugnito!" << std::endl;//NULL
-    }
+    std::string emettereVerso() const override;
+    const std::string setNome();
+    std::string setDescrizione();
 
     // METODI
 };
